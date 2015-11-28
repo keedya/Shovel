93efd39dd20377efcd0c1ff8c69c3c6696612767
keedya <andre.keedy@emc.com>
2015-11-28 13:24:49 -0500

    Update Ironic node extra data in poller

--- Shovel/lib/api/services/poller.js
+++ Shovel/lib/api/services/poller.js
@@ -4,9 +4,9 @@ var Promise = require('bluebird');
 
 module.exports.startPoller = function startPoller() {
     var ironic_client;
-    var time_interval;
+    var timeInterval;
 
-    time_interval = 20000;
+    timeInterval = 20000;
     setInterval(function () {
         return new Promise(function (resolve, reject) {
             ironic.get_client(function (client) {
@@ -18,25 +18,25 @@ module.exports.startPoller = function startPoller() {
         }).then(function (result) {
                 var ironic_nodes = JSON.parse(result).nodes;
                 for (var i in ironic_nodes) {
-                    console.info('Running poller on :' + ironic_nodes[i].uuid + '::::');
+                    console.log('Running poller on :' + ironic_nodes[i].uuid + ':');
                     ironic_client.get_node(ironic_nodes[i].uuid, function (node_data) {
                         node_data = JSON.parse(node_data);
                         if (node_data != undefined) {
                             if (node_data.error_message) {
                                 //continue;
                             }
-                            if (node_data.extra && node_data.extra.timer) {
+                            else if (node_data.extra && node_data.extra.timer) {
                                 //if timer stop is set to true stop the task
                                 if (node_data.extra.timer.stop) {
                                     // continue;
                                 }
                                 else {
-                                    var time_now = new Date();
-                                    var time_finished = node_data.extra.timer.finish;
-                                    time_interval = node_data.extra.timer.timeInteval;
-                                    var parsedDate = new Date(Date.parse(time_finished));
-                                    var newDate = new Date(parsedDate.getTime() + time_interval);
-                                    if (newDate < time_now) {
+                                    var timeNow = new Date();
+                                    var timeFinished = node_data.extra.timer.finish;
+                                    timeInterval = node_data.extra.timer.timeInterval;
+                                    var parsedDate = new Date(Date.parse(timeFinished));
+                                    var newDate = new Date(parsedDate.getTime() + timeInterval);
+                                    if (newDate < timeNow) {
                                         node_data.extra.timer.start = new Date().toJSON();
                                         if (node_data.extra.timer.isDone) {
                                             node_data.extra.timer.isDone = false;
@@ -55,19 +55,19 @@ module.exports.startPoller = function startPoller() {
     }, 5000);
 }
 function updateSelinfo(ironic_client, node_data) {
-    console.info(node_data.extra.nodeid);
+    console.log(node_data.extra.nodeid);
     return getSeldata(node_data.extra.nodeid).
         then(function (result) {
             if (result != undefined) {
-                console.info(result);
+                console.log(result);
                 result = JSON.parse(result);
                 if (result[0] && result[0].sel) {
-                    console.info('node_data.extra.eventcnt += 1');
+                    console.log('node_data.extra.eventcnt += 1');
                     node_data.extra.eventcnt += 1;
                 }
             }
             else {
-                console.info('request_poller_data_get return null => terminate task for :' + node_data.uuid);
+                console.log('request_poller_data_get return null => terminate task for :' + node_data.uuid);
                 return;
             }
             //update finish time
@@ -78,16 +78,16 @@ function updateSelinfo(ironic_client, node_data) {
                 result = JSON.parse(result);
                 if (result != undefined) {
                     if (result.error_message) {
-                        console.info('unregistered node');
+                        console.log('unregistered node');
                         return
                     }
                     if (result.extra) {
                         if (result.extra.stop) {
-                            console.info('task forced to stop');
-                            console.info();
+                            console.log('task forced to stop');
+                            console.log();
                             return;
                         }
-                        console.info(result.extra);
+                        console.log(result.extra);
                     }
                 }
                 return;
